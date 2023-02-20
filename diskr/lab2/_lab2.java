1c442f816c5dc9880e75ec742661e929b5044f8f
Zahar <111503336+ZaharSirko@users.noreply.github.com>
2023-02-20 22:09:47 +0200

    Update _lab2.java

--- diskr/lab2/_lab2.java
+++ diskr/lab2/_lab2.java
@@ -1,10 +1,6 @@
 package diskr.lab2;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+
 
 public class _lab2 {
     public static void main(String[] args) {
