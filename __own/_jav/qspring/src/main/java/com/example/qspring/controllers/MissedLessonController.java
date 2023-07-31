918a29965f9f4d3827211759f7346b52a77749e3
ZaharSirko <magezku@gmail.com>
2023-07-31 18:26:49 +0300

    update

---
+++ __own/_jav/qspring/src/main/java/com/example/qspring/controllers/MissedLessonController.java
@@ -0,0 +1,12 @@
+package com.example.qspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MissedLessonController {
+    @GetMapping("/missed_lessons")
+    public String groupMain(){
+      return "missed_lessons";
+    }
+}
